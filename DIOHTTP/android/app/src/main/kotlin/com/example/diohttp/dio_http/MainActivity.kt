a710522de7a0a5db52c65ca4eaac94d7ca2c1db9
최 강훈(kchoi) <kchoi@techtaka.com>
2022-06-06 18:45:25 +0900

    add: flutter declartive navigation

---
+++ DIOHTTP/android/app/src/main/kotlin/com/example/diohttp/dio_http/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diohttp.dio_http
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ DIOHTTP/lib/src/data.dart
@@ -0,0 +1,54 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'data.g.dart';
+/*
+// https://reqres.in/api/users/2
+
+{
+  "data": {
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+  },
+  "support": {
+    "url": "https://reqres.in/#support-heading",
+    "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+  }
+
+ */
+
+@JsonSerializable()
+class User {
+  User({
+    required this.data,
+  });
+
+  Data data;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
+
+@JsonSerializable()
+class Data {
+  Data({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  int id;
+  String email;
+  @JsonKey(name: 'first_name')
+  String firstName;
+  @JsonKey(name: 'last_name')
+  String lastName;
+  String avatar;
+
+  factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
+  Map<String, dynamic> toJson() => _$DataToJson(this);
+}
